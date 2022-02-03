5cf1b2afad18e223fc290d0f04c5bcc3be45577e
James Jewson <jamesjewson.dev@gmail.com>
2022-02-02 19:49:11 -0600

    post request test

--- views/src/apiCalls.js
+++ views/src/apiCalls.js
@@ -5,6 +5,7 @@ export const loginCall = async (userCredentials,dispatch)=>{
     dispatch({type:"LOGIN_START"})
     try{
         const res = await axios.post("/auth/login", userCredentials)
+        console.log("this");
         dispatch({type:"LOGIN_SUCCESS", payload:res.data});
  
     }catch(err){
