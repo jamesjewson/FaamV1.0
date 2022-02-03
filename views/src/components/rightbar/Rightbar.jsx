3b825f365090b4ceca3fc34b2b6379fd8a8d9403
James Jewson <jamesjewson.dev@gmail.com>
2022-02-02 20:42:47 -0600

    added global axios default

--- views/src/components/rightbar/Rightbar.jsx
+++ views/src/components/rightbar/Rightbar.jsx
@@ -40,7 +40,7 @@ export default function Rightbar({user}) {
 //Get profile user Photos    
     useEffect(() => {
         const showPhotos = async ()=>{
-                const res = await axios.get("/posts/photos/" + user?.username)
+                const res = await axios.get("https://faamserver.herokuapp.com/api/posts/photos/" + user?.username)
                 setPhotos(res.data.sort((p1,p2)=>{
                     return new Date(p2.createdAt) - new Date(p1.createdAt);
                 })
