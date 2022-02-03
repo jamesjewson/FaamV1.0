cded5ee4744c97afd24b3b58b1a34dfc8f28063c
James Jewson <jamesjewson.dev@gmail.com>
2022-02-02 19:37:36 -0600

    added / to apiCalls login call, changed index.html css href

--- views/src/apiCalls.js
+++ views/src/apiCalls.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const loginCall = async (userCredentials,dispatch)=>{
     dispatch({type:"LOGIN_START"})
     try{
-        const res = await axios.post("auth/login", userCredentials)
+        const res = await axios.post("/auth/login", userCredentials)
         dispatch({type:"LOGIN_SUCCESS", payload:res.data});
  
     }catch(err){
