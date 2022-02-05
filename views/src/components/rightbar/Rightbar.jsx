fdabc90db474811f259a3320275b90b2ec7a61a5
James Jewson <jamesjewson.dev@gmail.com>
2022-02-05 15:20:49 -0600

    uncommented some stuff that broke the build

--- views/src/components/rightbar/Rightbar.jsx
+++ views/src/components/rightbar/Rightbar.jsx
@@ -10,7 +10,7 @@ import Online from "../online/Online"
 
 export default function Rightbar({user}) {
 
-    const {user:currentUser} =  useContext(AuthContext)
+    const {user:currentUser, dispatch} =  useContext(AuthContext)
     const [followed,setFollowed] = useState(false)
     const [photos, setPhotos] = useState([])
     const [viewImg, setViewImg] = useState(false)
@@ -63,7 +63,7 @@ export default function Rightbar({user}) {
         try{
             if(followed){
                 await axios.put(`/users/${user._id}/unfollow`, {userId:currentUser._id})
-               dispatch({type:"UNFOLLOW", payload:user._id})
+                dispatch({type:"UNFOLLOW", payload:user._id})
             }else{
                 await axios.put(`/users/${user._id}/follow`, {userId:currentUser._id})
                 dispatch({type:"FOLLOW", payload:user._id})
