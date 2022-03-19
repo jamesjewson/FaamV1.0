cae44f711e6b0deaef1248539fb6b3aa41ba3891
James Jewson <jamesjewson.dev@gmail.com>
2022-03-18 20:21:40 -0500

    unhardcoded api_key. Learned env filesnot being grabbed correctly

--- api/middleware/cloudinary.js
+++ api/middleware/cloudinary.js
@@ -4,8 +4,7 @@ require("dotenv").config({ path: "./.env" });
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  // api_key: process.env.API_KEY,
-  api_key: "415995813342542",
+  api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
   cloudinary_url: process.env.CLOUDINARY_URL
 });
