030e6e59fc6f3a14d195a26d6183aeff04c81bed
James Jewson <jamesjewson.dev@gmail.com>
2022-06-17 08:55:57 -0500

    didn't change server stuff back whoops

--- views/src/index.js
+++ views/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import { AuthContextProvider } from "./context/AuthContext"
 
 //Axios global prefix
-// import axios from 'axios'
-// axios.defaults.baseURL = 'https://faamserver.herokuapp.com/api'
+import axios from 'axios'
+axios.defaults.baseURL = 'https://faamserver.herokuapp.com/api'
 
 
 ReactDOM.render(
