1ba3963e4a2e0aac803022dea55b433ff231167e
James Jewson <jamesjewson.dev@gmail.com>
2022-06-23 15:43:20 -0500

    login page resdesign

--- views/src/pages/login/Login.jsx
+++ views/src/pages/login/Login.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../context/AuthContext"
 import {CircularProgress} from "@material-ui/core"
 import {Link} from "react-router-dom"
 import { Info } from "@material-ui/icons"
+import faamLarge from '../../styleAssets/images/faamLarge.png'
 
 export default function Login() {
 
@@ -30,49 +31,65 @@ export default function Login() {
     
     return (
         <div className="login">
+            <div className="loginTriangle">
+                <div class="arrow-up-black"></div>
+                <div class="arrow-up-green"></div>
+            </div>
             <div className="loginWrapper">
-                <div className="loginFormContainer">
-                    {/* <span className="loginWelcome">Welcome to Faam!</span> */}
-                    <form className="loginBox" onSubmit={handleClick}>
-                        <input 
-                            placeholder="Use demo@demo.com" 
-                            type="email" 
-                            required 
-                            className="loginInput" 
-                            ref={email} 
-                        />
-                        <input 
-                            placeholder="Use password demodemo" 
-                            type="password" 
-                            required 
-                            minLength="4" 
-                            className="loginInput" 
-                            ref={password} 
-                        />
-                        <button 
-                            className="loginButton" 
-                            type="submit" 
-                            disabled={isFetching} >
-                            {isFetching ? <CircularProgress className="circular" size="25px"/> : "Log In"}
-                        </button>
-                        <span className="loginForgot" onClick={handleForgotPassword}>
-                            <a href="https://jamesjewson.netlify.app/#contact" target="_blank" rel="noreferrer">Forgot Password?</a>
-                        </span>
-                        <Link to="/register" className="loginInput">
-                            <button className="loginRegisterButton">
-                                {isFetching ? (<CircularProgress className="circular" size="25px"/>) : (
-                                    "Create a New Account"
-                                )}
-                            </button>
-                        </Link>
-                    </form>
+                <div className="loginContainer">
+                    <section className="loginLeft">
+                        <span className="loginWelcome">Welcome to</span>
+                        <h1 className="loginLogo">
+                            <Link to="/">
+                                <img className="loginLogoImg" src={faamLarge} alt="logo"/>
+                            </Link>
+                        </h1>
+                    </section>
+                    <section className="loginRight">
+                        <div className="loginFormContainer">
+                            <form className="loginBox" onSubmit={handleClick}>
+                                <input 
+                                    placeholder="Use demo@demo.com" 
+                                    type="email" 
+                                    required 
+                                    
+                                    className="loginInput" 
+                                    ref={email} 
+                                    />
+                                <input 
+                                    placeholder="Use password demodemo" 
+                                    type="password" 
+                                    required 
+                                    minLength="4" 
+                                    className="loginInput" 
+                                    ref={password} 
+                                    />
+                                <button 
+                                    className="loginButton" 
+                                    type="submit" 
+                                    disabled={isFetching} >
+                                    {isFetching ? <CircularProgress className="circular" size="25px"/> : "Log In"}
+                                </button>
+                                <span className="loginForgot" onClick={handleForgotPassword}>
+                                    <a href="https://jamesjewson.netlify.app/#contact" target="_blank" rel="noreferrer">Forgot Password?</a>
+                                </span>
+                                <Link to="/register" className="loginInput">
+                                    <button className="loginRegisterButton">
+                                        {isFetching ? (<CircularProgress className="circular" size="25px"/>) : (
+                                            "Create a New Account"
+                                            )}
+                                    </button>
+                                </Link>
+                            </form>
+                        </div>
+                    </section>
                 </div>
             </div>
             <div className="loginFooter">
-                <Info className="sidebarIcon aboutIcon" />
-                <span className="sidebarListItemText">
-                    <Link to="/about" class="sidebarListItemText">
-                        <span className="sidebarListItemText">About This Site</span>
+                <span className="sidebarListItemText loginFooterAboutLink">
+                    <Link to="/about" class="sidebarListItemText ">
+                        <Info className="sidebarIcon aboutIcon aboutIconLogin" />
+                        <span className="loginFooterAbout">About This Site</span>
                     </Link>
                 </span>
             </div>
